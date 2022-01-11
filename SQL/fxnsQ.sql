3e07f309ed754513723fd5bc52f01d2bd7e6e317
Ali Younes <royly84323@gmail.com>
2022-01-11 08:04:51 +0200

    added rent tran

--- SQL/fxnsQ.sql
+++ SQL/fxnsQ.sql
@@ -25,38 +25,34 @@ RETURNS int
 AS
 -- Return the refunded amount if succeeded
 BEGIN
-DECLARE @Cid int, @Reqid int, @toRefund float;
-BEGIN TRAN @T1;
+	DECLARE @Cid int, @Reqid int, @toRefund float;
+	BEGIN TRAN @T1;
 
-SET @Cid = (SELECT RI_CT_ID FROM TBLRENTINFO WHERE RI_RENT_ID = @RID) --get cid from rentinfo table
+		SET @Cid = (SELECT RI_CT_ID FROM TBLRENTINFO WHERE RI_RENT_ID = @RID) --get cid from rentinfo table
 
--- Tables included : ACCOUNT / REQUEST / RENTINFO
--- Update request status for the request with reqid and cid
-UPDATE TBLREQUEST
-SET REQ_STATUS = 1
-WHERE REQ_CT_ID = @Cid AND REQ_RI_RENT_ID = @RID
+		-- Tables included : ACCOUNT / REQUEST / RENTINFO
+		-- Update request status for the request with reqid and cid
+		UPDATE TBLREQUEST
+		SET REQ_STATUS = 1
+		WHERE REQ_CT_ID = @Cid AND REQ_RI_RENT_ID = @RID
 
--- Transfer money from admin account to customer
-set @toRefund = (SELECT RI_AMOUNT FROM TBLRENTINFO 
-				 WHERE RI_CT_ID = @Cid AND RI_RENT_ID = @RID) * (@PTG / 100);
+		-- Transfer money from admin account to customer
+		set @toRefund = (SELECT RI_AMOUNT FROM TBLRENTINFO 
+						WHERE RI_CT_ID = @Cid AND RI_RENT_ID = @RID) * (@PTG / 100);
 
-UPDATE TBLACCOUNT
-SET AC_BALANCE = AC_BALANCE - @toRefund
-WHERE AC_CARD_NUMBER = 1 --Admin account
-UPDATE TBLACCOUNT
-SET AC_BALANCE = AC_BALANCE + @toRefund
-WHERE AC_CARD_NUMBER = @CNB
+		UPDATE TBLACCOUNT
+		SET AC_BALANCE = AC_BALANCE - @toRefund
+		WHERE AC_CARD_NUMBER = 1 --Admin account
 
-UPDATE TBLREQUEST
-SET REQ_CONFIRMDATE = (SELECT GETDATE())
-WHERE REQ_CT_ID = @Cid AND REQ_RI_RENT_ID = @RID
+		UPDATE TBLACCOUNT
+		SET AC_BALANCE = AC_BALANCE + @toRefund
+		WHERE AC_CARD_NUMBER = @CNB
 
--- delete from REQUEST table according to RENT_ID
--- deleting according to REQID may not delete all refund requests
--- one or more requests may have same RENT_ID , but they'll have different REQID
+		UPDATE TBLREQUEST
+		SET REQ_CONFIRMDATE = (SELECT GETDATE())
+		WHERE REQ_CT_ID = @Cid AND REQ_RI_RENT_ID = @RID
 
-
-COMMIT TRAN T1
+	COMMIT TRAN T1
 
 RETURN @toRefund
 END
@@ -65,6 +61,44 @@ GO
 ---- END OF TRANSACTION AND APPROVAL
 
 
+/*
+FXN INPUT : Rental ID, Refund percentage
+FXN OUTPUT: Amount refunded
+FXN to approve and carry out a refund transaction
+*/
+
+GO
+DECLARE @T2 VARCHAR(20);
+SELECT @T2 = 'RentTransaction';
+GO
+
+CREATE FUNCTION fnRent (@Amount float, @CardNumber int)
+RETURNS int
+AS
+BEGIN
+	DECLARE @ResultValue int
+	BEGIN TRAN @T1;
+
+		UPDATE TBLACCOUNT
+		SET AC_BALANCE = AC_BALANCE + @Amount
+		WHERE AC_CARD_NUMBER = 1;
+
+		UPDATE TBLACCOUNT
+		SET AC_BALANCE = AC_BALANCE - @Amount
+		WHERE AC_CARD_NUMBER = @CardNumber;
+		IF @ResultValue <> 0
+			BEGIN
+				PRINT 'Transaction Failure'
+				ROLLBACK TRAN
+			END
+		ELSE
+			BEGIN
+				PRINT 'Transaction succeeded'
+                COMMIT TRAN T1;
+			END
+	RETURN @Amount;
+END
+GO
 
 
 
